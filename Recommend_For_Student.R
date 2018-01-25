eb024c855042a6a8e1f62671d4e438c50cdc3944
John-Saul <js4633@tc.columbia.edu>
2018-01-24 19:41:10 -0500

    Solving Student rec Problem

--- Recommend_For_Student.R
+++ Recommend_For_Student.R
@@ -1,1 +1,9 @@
 Find similar students (Neighbors)= weighted average of rankings of items they rated, sort.  IF completed, then move to second completed
+
+T2.SIM <- cosine(t_comb_matrix)
+diag(T2.SIM) <- NA
+head(rownames(T2.SIM[order(T2.SIM[last.unit,], decreasing = TRUE),]), n = 3)
+
+important_people <- rbind(t_comb_matrix[,"mh3736"],t_comb_matrix[,"jl4965"], t_comb_matrix[,"ma3694"])
+important_people[,1] <- important_people[,1] * .3399770
+order_values <- as.matrix(rowMeans(important_people))
